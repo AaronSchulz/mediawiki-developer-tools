86dd6db1555a9de1392ca535f1a4626344634743
Aaron Schulz <aschulz@wikimedia.org>
2019-10-08 10:50:11 -0700

    Fix use of "local" in unittests.sh

--- unittests.sh
+++ unittests.sh
@@ -3,9 +3,9 @@ ARGS="$@"
 ./sync.sh
 (
   # Use Git for Windows git for the WIN10 path
-  local CHANGES=$(git.exe diff --name-only)
+  CHANGES=$(git.exe diff --name-only)
   if [ -n "${CHANGES}" ]; then
-    echo "The following core file(s) have uncommitted changes in git"
+    echo "The following core file(s) have uncommitted changes in git:"
     echo "${CHANGES}"
     exit 1
   fi
