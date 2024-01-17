60eee0a7144caa9841ca17bc0beca078cabf606b
Aaron Schulz <aschulz@wikimedia.org>
2024-01-17 12:45:37 -0800

    Whitespace cleanups

--- config/ParatestSettings.php
+++ config/ParatestSettings.php
@@ -19,7 +19,7 @@ function mwptInitSchemaClone( string $liveDbPath, string $cloneDbPath ) {
 	if ( !$stat['size'] || ( time() - $stat['mtime'] ) > 15 ) {
 		ftruncate( $fh, 0 );
 
-        $dbl = mwptSqliteHandle( $liveDbPath );
+		$dbl = mwptSqliteHandle( $liveDbPath );
 		$dbc = mwptSqliteHandle( $cloneDbPath );
 
 		$sqls = $dbl->selectFieldValues(
@@ -43,65 +43,65 @@ function mwptInitSchemaClone( string $liveDbPath, string $cloneDbPath ) {
 }
 
 function mwptSqliteHandle( $filename ) {
-    $p['dbFilePath'] = $filename;
-    $p['schema'] = null;
-    $p['tablePrefix'] = '';
-    $p['trxMode'] = 'IMMEDIATE';
+	$p['dbFilePath'] = $filename;
+	$p['schema'] = null;
+	$p['tablePrefix'] = '';
+	$p['trxMode'] = 'IMMEDIATE';
 
-    return ( new DatabaseFactory() )->create( 'sqlite', $p );
+	return ( new DatabaseFactory() )->create( 'sqlite', $p );
 }
 
 function mwptModifyDirectories() {
-    global $wgTmpDirectory, $wgSQLiteDataDir, $wgUploadDirectory, $wgCacheDirectory;
-    global $wgDBservers, $wgDBtype, $wgDBserver, $wgDBname, $wgDBuser, $wgDBpassword;
-    global $wgLocalisationCacheConf;
-    global $wgDirectoryMode;
-    global $wgDebugLogFile;
+	global $wgTmpDirectory, $wgSQLiteDataDir, $wgUploadDirectory, $wgCacheDirectory;
+	global $wgDBservers, $wgDBtype, $wgDBserver, $wgDBname, $wgDBuser, $wgDBpassword;
+	global $wgLocalisationCacheConf;
+	global $wgDirectoryMode;
+	global $wgDebugLogFile;
 
 	$envTmpDir = getenv( 'TMPDIR' );
 	$envTestToken = getenv( 'TEST_TOKEN' );
 	$envTestRunNo = getenv( 'TEST_RUN_NO' );
-    if ( in_array( false, [ $envTmpDir, $envTestToken, $envTestRunNo ], true ) ) {
-        // Not an invocation from paratest runner
-        return;
-    }
+	if ( in_array( false, [ $envTmpDir, $envTestToken, $envTestRunNo ], true ) ) {
+		// Not an invocation from paratest runner
+		return;
+	}
 
-    // Use the temp dir for the slot assigned by paratest
-    $wgTmpDirectory = "${envTmpDir}/${envTestToken}";
-    // Make slot directory
-    @mkdir( $wgTmpDirectory, $wgDirectoryMode );
-    // Set logger directory for unit tests
-    $wgDebugLogFile = "$wgTmpDirectory/mw-debug-cli.log";
+	// Use the temp dir for the slot assigned by paratest
+	$wgTmpDirectory = "${envTmpDir}/${envTestToken}";
+	// Make slot directory
+	@mkdir( $wgTmpDirectory, $wgDirectoryMode );
+	// Set logger directory for unit tests
+	$wgDebugLogFile = "$wgTmpDirectory/mw-debug-cli.log";
 
-    // Normalize $wgDBservers if not set
-    if ( $wgDBtype === 'sqlite' && !is_array( $wgDBservers ) ) {
-        $wgDBservers = [ [
-            'host' => $wgDBserver,
-            'dbname' => $wgDBname,
-            'user' => $wgDBuser,
-            'password' => $wgDBpassword,
-            'dbDirectory' => $wgSQLiteDataDir,
-            'type' => $wgDBtype
-        ] ];
-    }
-    // Make $wgDBservers use the assigned temp dir where applicable
-    if ( is_array( $wgDBservers ) ) {
-        foreach ( $wgDBservers as $i => $server ) {
-            if ( $server['type'] !== 'sqlite' ) {
-                continue;
-            }
-            // Create or reuse the empty test DB for the assigned slot
-            $liveDbPath = ( $server['dbDirectory'] ?? $wgSQLiteDataDir ) . "/$wgDBname.sqlite";
-            $cloneDbPath = "$wgTmpDirectory/$wgDBname.sqlite";
-            mwptInitSchemaClone( $liveDbPath, $cloneDbPath );
-            // Avoid sharing certain resources among concurrent threads
-            $wgDBservers[$i]['dbDirectory'] = $wgTmpDirectory;
-            // Use the test DB with relaxed settings
-            $wgDBservers[$i]['variables']['temp_store'] = 'MEMORY';
-            $wgDBservers[$i]['variables']['synchronous'] = 'OFF';
-            $wgDBservers[$i]['variables']['journal_mode'] = 'MEMORY';
-        }
-    }
+	// Normalize $wgDBservers if not set
+	if ( $wgDBtype === 'sqlite' && !is_array( $wgDBservers ) ) {
+		$wgDBservers = [ [
+			'host' => $wgDBserver,
+			'dbname' => $wgDBname,
+			'user' => $wgDBuser,
+			'password' => $wgDBpassword,
+			'dbDirectory' => $wgSQLiteDataDir,
+			'type' => $wgDBtype
+		] ];
+	}
+	// Make $wgDBservers use the assigned temp dir where applicable
+	if ( is_array( $wgDBservers ) ) {
+		foreach ( $wgDBservers as $i => $server ) {
+			if ( $server['type'] !== 'sqlite' ) {
+				continue;
+			}
+			// Create or reuse the empty test DB for the assigned slot
+			$liveDbPath = ( $server['dbDirectory'] ?? $wgSQLiteDataDir ) . "/$wgDBname.sqlite";
+			$cloneDbPath = "$wgTmpDirectory/$wgDBname.sqlite";
+			mwptInitSchemaClone( $liveDbPath, $cloneDbPath );
+			// Avoid sharing certain resources among concurrent threads
+			$wgDBservers[$i]['dbDirectory'] = $wgTmpDirectory;
+			// Use the test DB with relaxed settings
+			$wgDBservers[$i]['variables']['temp_store'] = 'MEMORY';
+			$wgDBservers[$i]['variables']['synchronous'] = 'OFF';
+			$wgDBservers[$i]['variables']['journal_mode'] = 'MEMORY';
+		}
+	}
 
 	// Make $wgLocalisationCacheConf use the assigned temp dir where applicable
 	$wgLocalisationCacheConf['storeDirectory'] = $wgTmpDirectory;
@@ -113,10 +113,10 @@ function mwptModifyDirectories() {
 		$wgLocalisationCacheConf['dbDirectory'] = $wgTmpDirectory;
 	}
 
-    // Avoid sharing certain resources among concurrent threads
-    $wgUploadDirectory = $wgTmpDirectory;
-    $wgCacheDirectory = $wgTmpDirectory;
-    $wgSQLiteDataDir = $wgTmpDirectory;
+	// Avoid sharing certain resources among concurrent threads
+	$wgUploadDirectory = $wgTmpDirectory;
+	$wgCacheDirectory = $wgTmpDirectory;
+	$wgSQLiteDataDir = $wgTmpDirectory;
 }
 
 mwptModifyDirectories();
